57cf768746253126e5a931c8dd280d7a112dfafa
kcalderon87 <kcalderon87@gmail.com>
2017-01-26 09:52:23 -0500

    inital upload

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+-- Insert a set of records.
+INSERT INTO burgers (burger_name, devoured) VALUES ('Taco Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Turkey Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Veggie Burger', false);
