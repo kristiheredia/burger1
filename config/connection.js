27fe2f308f74abe585233d2d94c6093b5cc27084
Kristi Heredia <froglander@dukworld.net>
2017-01-26 21:29:54 -0500

    Fix connection

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,18 @@
 var mysql = require('mysql');
 
+// var connection = mysql.createConnection({
+// 	host: 'localhost',
+// 	user: 'root',
+// 	password: '',
+// 	database: 'burgers_db'
+// })
+
 var connection = mysql.createConnection({
-	host: 'localhost',
-	user: 'root',
-	password: '',
-	database: 'burgers_db'
-})
+    host: 'mysql://rrzo5uvqkg9ou3z4:psv6i2qnbf3ercre@wp433upk59nnhpoh.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/tqgvvcx98zoxayrf',
+    user: 'rrzo5uvqkg9ou3z4',
+    password: 'psv6i2qnbf3ercre',
+    database: 'burgers_db'
+});
 
 connection.connect(function(err) {
 	if (err) {
@@ -14,6 +21,6 @@ connection.connect(function(err) {
 	}
 
 	console.log('connected to the database thread ID: ' + connection.threadId);
-})
+});
 
 module.exports = connection;
